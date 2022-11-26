a2568234b1d8e830905f741a06d6e754aa36c016
Vika <vikkiblossom@gmail.com>
2022-11-26 17:45:02 +0300

    Add Task 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());    
+}
+if (n >= 1 && n <= 5)
+    Console.WriteLine("нет");
+else if (n == 6 || n == 7)
+    Console.WriteLine("да");
+
