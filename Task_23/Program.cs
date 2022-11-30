6b498708205cf25ec7ed448b87f4820346d88021
Vika <vikkiblossom@gmail.com>
2022-11-30 11:06:33 +0300

    Change Tast 23

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -2,4 +2,4 @@
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i <= n; i++)
-    Console.WriteLine(Math.Pow(i, 3));
+    Console.Write($"{Math.Pow(i, 3)} ");
