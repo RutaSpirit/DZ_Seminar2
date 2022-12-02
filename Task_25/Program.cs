471ddfe20c730c4f947ec19335fae5e8f7274ccb
Vika <vikkiblossom@gmail.com>
2022-12-02 13:55:35 +0300

    add task 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= m; i++)
+result *= n; // result = result * n
+
+Console.WriteLine($"Результат: {result}");
