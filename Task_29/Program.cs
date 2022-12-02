af2fbd9dff65945c022724079edd706c0f5cfede
Vika <vikkiblossom@gmail.com>
2022-12-02 14:20:26 +0300

    add task 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write("Введите элемент массива: ");
+int x = Convert.ToInt32(Console.ReadLine());
+array[i] = x;
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
