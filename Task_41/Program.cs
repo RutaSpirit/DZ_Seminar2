8564e80380caefdb44eec56f9acee6d3a6c3918e
Vika <vikkiblossom@gmail.com>
2022-12-13 11:06:51 +0300

    Add Task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+string[] n = Console.ReadLine().Split(" ");
+int count = 0;
+foreach (string i in n)
+{
+    if (Convert.ToInt32(i) > 0) 
+       count++;
+}
+Console.WriteLine(count);
